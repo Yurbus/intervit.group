bff5b4732ed1d2c635c6d81aae08424e64d300c8
Yurbus <frankins636@gmail.com>
2024-07-17 10:51:55 +0300

    sdsds

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,7 @@
 
 const popupLinks = document.querySelectorAll('.popup-link');
 const body = document.querySelector('body');
-const lockPadding = document.querySelectorAll(".lock_padding");
+const lockPadding = document.querySelectorAll(".lock-padding");
 
 let unlock = true;
 
@@ -58,10 +58,10 @@ function bodyLock() {
     const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
 
 	if (lockPadding.length > 0) {
-    for (let index = 0; index < lockPadding.length; index++) {
-        const el = lockPadding[index];
-        el.style.paddingRight = lockPaddingValue;
-  	  }
+        for (let index = 0; index < lockPadding.length; index++) {
+            const el = lockPadding[index];
+            el.style.paddingRight = lockPaddingValue;
+        }
 	}
     body.style.paddingRight = lockPaddingValue;
     body.classList.add('lock');
